be786afbe7b2f3b2c03822fbe98e08e414202d0d
Kirill <kirillfromhome@gmail.com>
2025-06-05 09:37:28 +0300

    Update settings.py

--- exchangerate/settings.py
+++ exchangerate/settings.py
@@ -27,7 +27,8 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['localhost',
                  '127.0.0.1',
-                 '0.0.0.0',]
+                 '0.0.0.0',
+                 '*', ]
 
 CACHES = {
     'default': {
